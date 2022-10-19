7b50d5f6145dbabbb6b8bac09462dec1fa34d569
marcs-feh <marcosfehlauer@protonmail.com>
2022-10-19 14:56:28 -0300

    init

---
+++ init.el
@@ -0,0 +1,13 @@
+;; UI Options
+
+(setq inhibit-startup-message t) ; Disable startup message
+(scroll-bar-mode -1) ; Disable scrollbar
+(tool-bar-mode -1) ; Disable toolbar
+(menu-bar-mode -1) ; Disable menu bar
+(setq visible-bell nil) ; Disable visual bell
+(global-display-line-numbers-mode) ; Display line numbers
+(column-number-mode) ; Display column numbers
+(setq-default tab-width 2) ; Set tab to 2
+(setq frame-resize-pixelwise t) ; Allow Emacs' window to be any size and not snap to characters
+(fringe-mode '(16 . 24)) ; Lateral spaces
+
