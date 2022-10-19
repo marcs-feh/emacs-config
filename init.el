8e3782b08b7eb0f32e9d9eddc331c796f2bc58fe
marcs-feh <marcosfehlauer@protonmail.com>
2022-10-19 15:09:06 -0300

    initialize package manager

--- init.el
+++ init.el
@@ -25,10 +25,42 @@
 ; Load theme
 (load-theme 'wombat t)
 
-;; General options
+;; General Options
 
 ; Make so that ESC can quit prompts. Disable auto-save and backup files.
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (setq auto-save-default nil)
 (setq make-backup-files nil)
 
+;; Package Manager
+
+; Set package sources
+(require 'package)
+(setq package-archives
+	'(("melpa" . "https://melpa.org/packages/")
+		("org"   . "https://orgmode.org/elpa/")
+		("elpa"  . "https://elpa.gnu.org/packages")))
+
+; Initialize package management
+(package-initialize)
+(unless package-archive-contents
+	(package-refresh-contents))
+
+; Require use-package for better management
+(unless (package-installed-p 'use-package)
+	(package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
